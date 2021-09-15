033b538ff6641ab6c95003a93a65784caf26e3a5
AndrewAmore <awamore@gmail.com>
2021-09-14 22:11:35 -0400

    removed extra files

--- data-cleaning.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(ggplot2)
-library(caret)
-
-all <- read.csv("sonoma-data-all.csv")
-log <- read.csv("sonoma-data-log.csv")
-net <- read.csv("sonoma-data-net.csv")
-dates <- read.csv("sonoma-data-net.csv")
